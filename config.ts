b5e8cf02ca500354945d9ea2f5b43d078e033242
otchoumouAng <otchoumou.ang@gmail.com>
2025-12-15 14:45:12 +0000

    ok

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
 //export const baseUrl = 'http://192.168.154.38:5104/api';  // -> wifi mobil
-//export const baseUrl = 'http://192.168.1.3:5104/api';  // -> wifi 
+export const baseUrl = 'http://192.168.1.7:5104/api';  // -> wifi 
 //export const baseUrl = 'http://10.0.2.2:5104/api'; // -> adresse Emulateur
-export const baseUrl = 'https://apisitc.odmtec.com/api'; // -> adresse Prod
+//export const baseUrl = 'https://apisitc.odmtec.com/api'; // -> adresse Prod
